e0572eae6b967b61bd47fb2e37179f68185d1e03
ivan <bochenkoivan@gmail.com>
2023-07-21 20:59:44 +0300

    photo

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,6 @@ const region = process.env.AWS_REGION
 
 const s3 = new S3Client({
   region,
-  logger: console,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
