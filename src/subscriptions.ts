2f76eec22dea75c65ed8044c6ca458487f47d0d7
ivan <bochenkoivan@gmail.com>
2023-09-20 17:16:45 +0300

    t

--- src/subscriptions.ts
+++ src/subscriptions.ts
@@ -3,21 +3,20 @@ import { dbClient } from "."
 import { notifyUsersInChat } from "./notifications"
 
 export const subscriptionRoute = new Elysia()
-  .ws('/chat/:event_id/:author_id', {
+  .ws('/chat/:event_id', {
     params: t.Object({
       event_id: t.String()
     }),
     body: t.Object({
       text: t.String(),
       author_id: t.String(),
-      event_id: t.String(),
     }),
     open(ws) {
       ws.subscribe(ws.data.params.event_id)
     },
     async message(ws, message) {
       const newMessage = await dbClient.message.create({
-        data: message,
+        data: { ...message, event_id: ws.data.params.event_id },
         include: {
           author: {
             select: {
@@ -29,8 +28,8 @@ export const subscriptionRoute = new Elysia()
           }
         }
       })
-      ws.publish(message.event_id, newMessage)
-      await notifyUsersInChat(message.event_id, newMessage)
+      ws.publish(ws.data.params.event_id, newMessage)
+      await notifyUsersInChat(ws.data.params.event_id, newMessage)
     },
     close(ws) {
       ws.unsubscribe(ws.data.params.event_id)
