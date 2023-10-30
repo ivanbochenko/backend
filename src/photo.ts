88632c022c0e897eefbd693d4641f1165826ccdd
ivan <bochenkoivan@gmail.com>
2023-10-30 14:13:44 +0200

    s

--- src/photo.ts
+++ src/photo.ts
@@ -23,7 +23,7 @@ export const photoRoute = new Elysia()
         Bucket,
         Key,
         Expires,
-        Body,
+        Body: file.stream(),
         ContentType: file.type,
       })
       try {
